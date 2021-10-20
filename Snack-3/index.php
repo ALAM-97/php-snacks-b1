e5546af9202922ba6884e4efcff970768feb321c
ALAM-97 <alexamara97@gmail.com>
2021-10-20 12:15:07 +0200

    Snack-3 Completed

---
+++ Snack-3/index.php
@@ -0,0 +1,67 @@
+<?php
+
+$posts = [
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 3</title>
+</head>
+
+<body>
+    <?php foreach ($posts as $key => $postContent) { ?>
+        <h2><?php echo $key ?></h2>
+        <ul>
+            <?php foreach ($postContent as $info) { ?>
+                <h4> <?php echo $info["title"]; ?> </h4>
+                <div> <?php echo $info["author"]; ?> </div>
+                <p> <?php echo $info["text"]; ?> </p>
+            <?php } ?>
+        </ul>
+    <?php } ?>
+</body>
+
+</html>
